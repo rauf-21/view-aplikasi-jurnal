3e2f5064947fc233669de2c5275cc5b7169ee9dd
rauf-21 <rauf21dev@gmail.com>
2021-07-16 10:50:17 +0700

    Fix previous commit

---
+++ src/composables/useAlert.js
@@ -0,0 +1,36 @@
+import Swal from 'sweetalert2/src/sweetalert2.js';
+import { ref } from 'vue';
+
+const useAlert = () => {
+  const showConfirmation = ref(true);
+
+  async function warningAlert (customOptions = { title: 'Content', text: 'Text' }) { 
+    const options = Object.assign({
+      icon: 'warning',
+      showConfirmButton: true,
+      showCancelButton: true,
+      iconColor: '#e74c3c',
+      confirmButtonColor: '#e74c3c',
+      cancelButtonColor: '#19191A'
+    }, customOptions);
+  
+    const { value: response } = showConfirmation.value ? await Swal.fire(options) : { value: true };
+    
+    if (response !== undefined) {
+      if (response) {
+        showConfirmation.value = false;
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  return {
+    showConfirmation,
+    warningAlert
+  }
+}
+
+
+export default useAlert;
